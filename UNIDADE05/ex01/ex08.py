9a265dfd42b208c43fc2fd149998478991108892
marloon-dev <marloon.dev@gmail.com>
2026-05-28 17:34:48 +0100

    Atualizado

--- UNIDADE05/ex01/ex08.py
+++ UNIDADE05/ex01/ex08.py
@@ -1,12 +1,12 @@
 def criar_arquivo(nome_arquivo, conteudo):
     try:
         with open(nome_arquivo, "w") as arquivo:
-        arquivo.write(conteudo)
-    print(f"O arquivo "{nome_arquivo}" foi criado com sucesso!")
+            arquivo.write(conteudo)
+        print(f"O arquivo {nome_arquivo} foi criado com sucesso!")
     except Exception as e:
-    print(f"Erro ao criar o arquivo: {e}")
+        print(f"Erro ao criar o arquivo: {e}")
 
-nome_arquivo = "MeuArquivo.txt"
+nome_arquivo = "UNIDADE05/ex01/MeuArquivo.txt"
 conteudo = "Python s2."
 
-criar_arquivo_txt(nome_arquivo, conteudo)
+criar_arquivo(nome_arquivo, conteudo)
