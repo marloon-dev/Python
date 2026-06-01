592406c487a925194ea36700128487dc53917ce3
marloon-dev <marloon.dev@gmail.com>
2026-06-01 19:59:34 +0100

    v1.6

--- Gustavo Guanabara Python/Desafios/des014.py
+++ Gustavo Guanabara Python/Desafios/des014.py
@@ -1,1 +1,15 @@
+print("\n==== CONVERSOR DE CELSIUS EM FAHRENHEIT====\n")
+
+# INPUT PARA DIGITAR OS GRAUS CELSIUS
 graus_celsius = float(input("Temperatura em °C: "))
+
+# CONVERSERTE CELSIUS EM FAHRENHEIT
+graus_fahrenheit = (graus_celsius * 1.8) + 32
+
+# MOSTRAR NA TELA RESULTADO
+print(
+    "\nGraus Celsius: {:.2f}°C".format(graus_celsius),
+    "\nGraus Fahrenheit: {:.2f}°F".format(graus_fahrenheit)
+)
+
+print("\n==== Fim do programa ====\n")
