b6423ccbe8f96c85a9811e40f0c695a9a96e318b
marloon-dev <marloon.dev@gmail.com>
2026-06-10 00:03:21 +0100

    V3.6

---
+++ Gustavo Guanabara Python/Desafios/des025.py
@@ -0,0 +1,8 @@
+print("\n==== EXERCÍCIO 25 ====")
+
+# DIGITE O NOME COMPLETO
+natural = input("\nNOME COMPLETO: ")
+natural = natural.upper()
+print("NUNES" in natural)
+
+print("\n####################....100% PROGRAMA FINALIZADO....")
