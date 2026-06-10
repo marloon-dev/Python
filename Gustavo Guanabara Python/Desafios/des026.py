48c1b82984711e4d35e69d26d51558313fd63904
marloon-dev <marloon.dev@gmail.com>
2026-06-10 10:21:01 +0100

    v3.7

---
+++ Gustavo Guanabara Python/Desafios/des026.py
@@ -0,0 +1,8 @@
+print("\n==== EXERCÍCIO 26 ====")
+
+# DIGITE UMA FRASE
+frase = str(input("FRASE: "))
+frase = frase.strip()
+print(frase)
+
+print("\n####################....100% PROGRAMA FINALIZADO....")
