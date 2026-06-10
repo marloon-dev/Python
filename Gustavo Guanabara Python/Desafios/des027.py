710121bfca36eca7c883bd2c3f6aff58abf8189b
marloon-dev <marloon.dev@gmail.com>
2026-06-10 13:25:17 +0100

    V3.9

---
+++ Gustavo Guanabara Python/Desafios/des027.py
@@ -0,0 +1,13 @@
+print("\n==== EXERCÍCIO 27 ====")
+
+# DIGITE SEU NOME
+nome = str(input("\nNOME COMPLETO: ")).upper().strip().split()
+
+# IMPRIMIR NA TELA RESULTADOS
+print("\nPRAZER EM CONHECÊ-LO!!!")
+print("{}".format(nome))
+print("\nPRIMEIRO NOME: {}".format(nome[0]))
+print("ULTIMO NOME: {}".format(nome[-1]))
+print("NOME: {} {}".format(nome[0],nome[-1]))
+
+print("\n####################....100% PROGRAMA FINALIZADO....")
