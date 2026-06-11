cbc9f2b271e6f6d88b07002efe5b0430c717b497
marloon-dev <marloon.dev@gmail.com>
2026-06-11 11:10:46 +0100

    V3.12

---
+++ Gustavo Guanabara Python/Aulas/aula010a.py
@@ -0,0 +1,11 @@
+print("\n==== BOX KEYS ====")
+
+nome = str(input("NOME: ")).upper().strip()
+
+if nome[0] == "MARLON":
+    print("\nMOSTRAR SENHA PARA {}".format(nome))
+    print("\nSENHA: SADKADKAJ#@€@4144Ddsd")
+else:
+    print("USUÁRIO NÃO LOCALIZADO")
+
+print("\n####################....100% PROGRAMA FINALIZADO....")
