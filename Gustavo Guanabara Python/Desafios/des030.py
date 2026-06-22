a1ac9cd9a8405d590363353b044065fface8b398
marloon-dev <marloon.dev@gmail.com>
2026-06-22 12:23:04 +0100

    v3.16

---
+++ Gustavo Guanabara Python/Desafios/des030.py
@@ -0,0 +1,13 @@
+print("\n==== EXERCÍCIO 30 ====")
+
+print("\nDIGITE UM NÚMERO PARA SABER SE É IMPAR OU PAR")
+numero = int(input("\nNUMERO: "))
+
+if numero % 2 == 0:
+    print("\nRESULTADO")
+    print("\n{:.0f} É PAR".format(numero))
+else:
+    print("\nRESULTADO")
+    print("\n{:.0f} É IMPAR".format(numero))
+
+print("\n####################....100% PROGRAMA FINALIZADO....")
