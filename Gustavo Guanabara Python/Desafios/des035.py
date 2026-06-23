ebf345379df70f3afbb3f01867e4d83a4cfd296b
marloon-dev <marloon.dev@gmail.com>
2026-06-23 10:27:20 +0100

    v3.21

--- Gustavo Guanabara Python/Desafios/des035.py
+++ Gustavo Guanabara Python/Desafios/des035.py
@@ -1,9 +1,12 @@
 print("\n==== EXERCÍCIO 35 ====")
 
+
 reta_1 = float(input("RETA 01: "))
 reta_2 = float(input("RETA 02: "))
 reta_3 = float(input("RETA 03: "))
 
-
-
+if reta_1 < reta_2 + reta_3 and reta_2 < reta_1 + reta_3 and reta_3 < reta_1 + reta_2:
+    print("RETAS PODE VIRA TRIÂNGULO: SIM")
+else:
+    print("RETAS PODE VIRA TRIÂNGULO: NÃO")
 print("\n####################....100% PROGRAMA FINALIZADO....")
