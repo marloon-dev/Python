b6a1e11ecf2ddc09f740eb541c36b840865d5551
marloon-dev <marloon.dev@gmail.com>
2026-06-23 23:01:11 +0100

    v3.28

--- Gustavo Guanabara Python/MUNDO - 02/EXERCÍCIOS/ex036.py
+++ Gustavo Guanabara Python/MUNDO - 02/EXERCÍCIOS/ex036.py
@@ -9,7 +9,7 @@ def inicio():
     print(f'{ciano}=-' * 21 + reset)
     print(f'{azul}|          INICIANDO O PROGRAMA          |{reset}')
     print('|                                        |')
-    print(f'{verde}|       Bem-vindo aos exercícios! 🚀     |{reset}')
+    print(f'{verde}|            EXERCÍCIO - 36🚀            |{reset}')
     print('|                                        |')
     print(f'{verde}|          Bons estudos em Python        |{reset}')
     print(f'{ciano}=-' * 21 + reset)
@@ -27,6 +27,6 @@ def fim():
 
 inicio()
 
-# Seu código aqui
+
 
 fim()
