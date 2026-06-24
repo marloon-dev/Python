4163ea1b4bbc8d292acc6dd12ce1e926ecebb83e
marloon-dev <marloon.dev@gmail.com>
2026-06-24 17:15:34 +0100

    v3.34

--- Gustavo Guanabara Python/MUNDO - 02/EXERCÍCIOS/ex037.py
+++ Gustavo Guanabara Python/MUNDO - 02/EXERCÍCIOS/ex037.py
@@ -2,6 +2,7 @@
 ciano = "\033[36m"
 azul = "\033[34m"
 verde = "\033[32m"
+vermelho = "\033[31m"
 reset = "\033[m"
 
 
@@ -27,6 +28,34 @@ def fim():
 
 inicio()
 
+print(f"{ciano}=-" * 21 + reset)
+print(f"{reset}|   PREENCHA COM NUMERO PARA CONVERTER   |")
+print(f"{reset}|      [1] = CONVERTER PARA BINÁRIO      |")
+print(f"{reset}|      [2] = CONVERTER PARA OCTAL        |")
+print(f"{reset}|      [3] = CONVERTER PARA HEXADECIMAL  |")
 
+print(f"{ciano}=-" * 21 + reset)
 
+while True:
+    opcao = int(input("\n{}OPÇÃO: ".format(ciano)))
+
+    if opcao in (1, 2, 3):
+          break
+    print("{}OPÇÃO INVÁLIDA! DIGITE 1: 2 OU 3.".format(vermelho))
+      
+numero = int(input("{}NÚMERO: ".format(ciano)))
+
+if opcao == 1:
+        resultado = bin(numero)[2:]
+
+elif opcao == 2:
+    resultado = oct(numero)[2:]
+
+elif opcao == 3:
+        resultado = hex(numero)[2:].upper()
+       
+
+print("{}RESULTADO: {}".format(verde, resultado))
+
+print("")
 fim()
