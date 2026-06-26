d494f49bb96136093450c769ea2ad4c148bcdcf1
marloon-dev <marloon.dev@gmail.com>
2026-06-26 10:42:47 +0100

    Update ex042.py
    v3.41

--- Gustavo Guanabara Python/MUNDO - 02/EXERCÍCIOS/ex042.py
+++ Gustavo Guanabara Python/MUNDO - 02/EXERCÍCIOS/ex042.py
@@ -12,7 +12,7 @@ def inicio():
     print(f"{ciano}=-" * 21 + reset)
     print(f"{azul}|          INICIANDO O PROGRAMA          |{reset}")
     print("|                                        |")
-    print(f"{verde}|            EXERCÍCIO - 36 🐍           |{reset}")
+    print(f"{verde}|            EXERCÍCIO - 42 🐍           |{reset}")
     print("|                                        |")
     print(f"{verde}|          Bons estudos em Python        |{reset}")
     print(f"{ciano}=-" * 21 + reset)
@@ -31,6 +31,30 @@ def fim():
 
 inicio()
 
+r_1 = int(input("{}1° Seguimento: {}".format(ciano, reset)))
+r_2 = int(input("{}2° Seguimento: {}".format(ciano, reset)))
+r_3 = int(input("{}3° Seguimento: {}".format(ciano, reset)))
 
+if r_1 < r_2 + r_3 and r_2 < r_1 + r_3 and r_3 < r_1 + r_2:
+    converte = "{}Sim".format(verde)
+else:
+    converte = "{}Não".format(vermelho)
+
+# SDAKDSA
+
+if converte == "{}Não".format(vermelho):
+    resultado = "{}Tipo inválido".format(vermelho)
+
+elif r_1 == r_2 and r_1 == r_3:
+    resultado = "Equilátero"
+
+elif r_1 == r_2 or r_1 == r_3 or r_2 == r_3:
+    resultado = "Isóceles"
+
+elif r_1 != r_2 or r_1 != r_3 or r_2 != r_3:
+    resultado = "Escaleno"
+
+print("\n{}Pode ser formar um triângulo: {}{}".format(ciano, reset, converte))
+print("{}Tipo de triângulo: {}{}".format(ciano, reset, resultado))
 
 fim()
