2707308ec9352a7534b39192fa0a068795c9a7d5
marloon-dev <marloon.dev@gmail.com>
2026-06-29 10:25:51 +0100

    v4.1

--- Gustavo Guanabara Python/MUNDO - 02/EXERCÍCIOS/ex044.py
+++ Gustavo Guanabara Python/MUNDO - 02/EXERCÍCIOS/ex044.py
@@ -12,7 +12,7 @@ def inicio():
     print(f"{ciano}=-" * 21 + reset)
     print(f"{azul}|          INICIANDO O PROGRAMA          |{reset}")
     print("|                                        |")
-    print(f"{verde}|            EXERCÍCIO - 36 🐍           |{reset}")
+    print(f"{verde}|            EXERCÍCIO - 44 🐍           |{reset}")
     print("|                                        |")
     print(f"{verde}|          Bons estudos em Python        |{reset}")
     print(f"{ciano}=-" * 21 + reset)
@@ -31,6 +31,32 @@ def fim():
 
 inicio()
 
+valor = float(input("\bVALOR DO PRODUTO: €"))
+print("\nFORMAS DE PAGAMENTO")
+print("[ 1 ] à vista dinheiro/cheque")
+print("[ 2 ] à vista cartão")
+print("[ 3 ] 2x cartão")
+print("[ 4 ] 3x ou mais cartão")
+opcao = int(input("QUAL É A OPÇÃO: "))
+if opcao == 4:
+    parcelas = str(input("QUANTIDADE DE PARCELAS: "))
+
+desc1 = valor * 0.10
+desc2 = valor * 0.05
+acres = valor * 0.20
+if opcao == 1:
+    resultado = valor - desc1
+elif opcao == 2:
+    resultado = valor - desc2
+elif opcao == 3:
+    resultado = valor
+elif opcao == 4:
+    resultado = valor + acres
+
+valor_parcelas = parcelas / resultado
+
+print("VALOR DA COMPRA: {}".format(resultado))
+print("PARCELAS: {}x de € {}".format(parcelas(parcelas,valor_parcelas )))
 
 
 fim()
