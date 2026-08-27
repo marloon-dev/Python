5785b2fb2f0f343128448865add4fb6097c575f6
marloon-dev <marloon.dev@gmail.com>
2026-08-27 10:18:34 +0100

    v5.1

--- Gustavo Guanabara Python/MUNDO - 02/EXERCÍCIOS/ex050.py
+++ Gustavo Guanabara Python/MUNDO - 02/EXERCÍCIOS/ex050.py
@@ -31,7 +31,15 @@ def fim():
 
 inicio()
 
+soma = 0
+cont = 0
+
 for c in range(1, 7):
-    num = int(input("DIGITE UM VALOR: "))
+    num = int(input("DIGITE {}º VALOR: ".format(c)))
+    if num % 2 == 0:
+        soma += num
+        cont += 1
+        
+print("Você informou {} números pares e a soma foi {}".format(cont, soma))
 
 fim()
